3c04868c2ec3457d1fc41336b56e03c805e20b46
Piraveena <piraveena18@gmail.com>
2017-09-01 09:35:23 +0530

    new value for sid and change in pom

--- src/main/java/org/wso2/identity/sample/ClaimAdderImp.java
+++ src/main/java/org/wso2/identity/sample/ClaimAdderImp.java
@@ -1,5 +1,6 @@
 package org.wso2.identity.sample;
 
+import com.hazelcast.web.SessionState;
 import org.wso2.carbon.identity.oauth2.ClaimAdder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -7,6 +8,7 @@ import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.authz.OAuthAuthzReqMessageContext;
 import org.wso2.carbon.identity.oauth2.dto.OAuth2AuthorizeRespDTO;
 import org.wso2.carbon.identity.oauth2.model.HttpRequestHeader;
+import org.wso2.carbon.identity.oidc.session.OIDCSessionConstants;
 import org.wso2.carbon.identity.oidc.session.OIDCSessionState;
 import org.wso2.carbon.identity.oidc.session.util.OIDCSessionManagementUtil;
 
@@ -15,6 +17,7 @@ import org.wso2.carbon.identity.oidc.session.util.OIDCSessionManagementUtil;
 import javax.servlet.http.Cookie;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 
 public class ClaimAdderImp implements ClaimAdder {
@@ -22,6 +25,8 @@ public class ClaimAdderImp implements ClaimAdder {
     private static Log log = LogFactory.getLog(ClaimAdder.class);
     private int claimValue;
     private String name;
+    private Cookie obpsCookie;
+    private String value;
 
 
     @Override
@@ -32,46 +37,33 @@ public class ClaimAdderImp implements ClaimAdder {
         Map<String, Object> addtionalClaims = new HashMap<>();
 
         this.name = "sid";
-        Cookie opbsCookie= oAuthAuthzReqMessageContext.getAuthorizationReqDTO().getObpscookie();
-        if (opbsCookie == null) {
-            log.info("no obps cookie");
-            this.claimValue = (int) (Math.random() * 1000000 + 10000000L);
+        if (getSessionState(oAuthAuthzReqMessageContext) == null) {
+       //     this.claimValue = (int) (Math.random() * 1000000 + 10000000L);
+            String value= UUID.randomUUID().toString();
             log.info("claim sid: " + claimValue);
-
-
-        }
-        else {
-            OIDCSessionState previousSessionState =
-                    OIDCSessionManagementUtil.getSessionManager()
-                            .getOIDCSessionState(opbsCookie.getValue());
-            claimValue=previousSessionState.getSidClaim();
+        } else {
+            claimValue = getSessionState(oAuthAuthzReqMessageContext).getSidClaim();
         }
 
-
-//        for( HttpRequestHeader header: requestHeaders) {
-//            St
-// ring name = header.getName();
-//            log.info(name);
-//            if (header.getName().equals("cookie")) {
-//                String[] values = header.getValue();
-//                for (String value : values) {
-//                    if (!value.contains("opbs")) {
-//                        this.claimValue = (int) (Math.random() * 1000000 + 10000000L);
-//                        log.info("claim sid: "+ value);
-//                        this.name="sid";
-//                        break;
-//
-//
-//                    }
-//                    else{
-//                        Sess
-//                    }
-//                }
-//            }
-
-        addtionalClaims.put(name, claimValue);
+        addtionalClaims.put(name, value);
         return addtionalClaims;
 
 
+    }
+
+    public OIDCSessionState getSessionState(OAuthAuthzReqMessageContext oAuthAuthzReqMessageContext) {
+
+        Cookie[] cookies = oAuthAuthzReqMessageContext.getAuthorizationReqDTO().getCookie();
+        for (Cookie cookie : cookies) {
+
+            if (cookie.getName().equals(OIDCSessionConstants.OPBS_COOKIE_ID)) {
+                OIDCSessionState previousSessionState = OIDCSessionManagementUtil.getSessionManager()
+                        .getOIDCSessionState(cookie.getValue());
+                return previousSessionState;
+
+            }
+
+        }
+        return null;
     }
 }
